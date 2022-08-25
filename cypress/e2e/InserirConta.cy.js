fe1ac6dc10958dcca9d0ffbf630cbe1dab9d773b
Fabio <fabio.soaresf20@gmail.com>
2022-08-25 10:06:39 -0300

    Commit

--- cypress/e2e/InserirConta.cy.js
+++ cypress/e2e/InserirConta.cy.js
@@ -1,5 +1,17 @@
-describe('empty spec', () => {
-  it('passes', () => {
-    cy.visit('https://example.cypress.io')
+///  <reference types="cypress"/>
+
+describe('Inserir uma conta', () => {
+  before(()=>{
+    cy.visit('http://barrigareact.wcaquino.me');
+    //cy.registrar(); --> Utilizado para registrar um usuario
+    cy.logar();
+  })
+  it('Inserindo conta', () => {
+    cy.get('[data-test="menu-settings"]').click();
+    cy.get('[href="/contas"]').click();
+    cy.get('[data-test="nome"]').type('Conta de luz')
+    cy.get('.btn').click()
+
+    cy.get('.toast-success > .toast-message').should('exist').contains('Conta inserida com sucesso!')
   })
 })
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,25 +1,16 @@
-// ***********************************************
-// This example commands.js shows you how to
-// create various custom commands and overwrite
-// existing commands.
-//
-// For more comprehensive examples of custom
-// commands please read more here:
-// https://on.cypress.io/custom-commands
-// ***********************************************
-//
-//
-// -- This is a parent command --
-// Cypress.Commands.add('login', (email, password) => { ... })
-//
-//
-// -- This is a child command --
-// Cypress.Commands.add('drag', { prevSubject: 'element'}, (subject, options) => { ... })
-//
-//
-// -- This is a dual command --
-// Cypress.Commands.add('dismiss', { prevSubject: 'optional'}, (subject, options) => { ... })
-//
-//
-// -- This will overwrite an existing command --
-// Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+Cypress.Commands.add('registrar', ()=>{
+    cy.get(':nth-child(2) > .nav-link').click();
+    cy.get('.jumbotron > :nth-child(1) > .form-control')
+      .type('Teste---Udemy');
+    cy.get('.input-group > .form-control')
+      .type('emaildosguri@gmail.com');
+    cy.get(':nth-child(3) > .form-control')
+      .type('teste123');
+    cy.get('.btn').click();
+})
+
+Cypress.Commands.add('logar', ()=>{
+    cy.get('[data-test="email"]').type('emaildosguri@gmail.com');
+    cy.get('[data-test="passwd"]').type('teste123');
+    cy.get('.btn').click();
+})
